8d294cf7d79dfa5c7430f58bd005aeb2ac57da88
boring1f6 <14566@havasuonline.org>
2020-10-10 16:16:06 -0700

    added contact form handlers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -25,10 +26,11 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery 
-            currentCategory={currentCategory}
-          ></Gallery>
-          <About></About>
+            <ContactForm></ContactForm>
+            <Gallery 
+                currentCategory={currentCategory}
+            ></Gallery>
+            <About></About>
         </div>
       </main>
     </div>
